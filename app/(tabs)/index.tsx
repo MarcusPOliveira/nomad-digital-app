43e9d0eba0a412c344ff1b4a2ce078b6462ae4e5
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2025-11-16 12:05:31 -0300

    refactor: clean up code structure and remove redundant sections

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -25,9 +25,8 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
+          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+          Press{" "}
           <ThemedText type="defaultSemiBold">
             {Platform.select({
               ios: "cmd + d",
@@ -45,11 +44,7 @@ export default function HomeScreen() {
           </Link.Trigger>
           <Link.Preview />
           <Link.Menu>
-            <Link.MenuAction
-              title="Action"
-              icon="cube"
-              onPress={() => alert("Action pressed")}
-            />
+            <Link.MenuAction title="Action" icon="cube" onPress={() => alert("Action pressed")} />
             <Link.MenuAction
               title="Share"
               icon="square.and.arrow.up"
@@ -74,11 +69,8 @@ export default function HomeScreen() {
         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
         <ThemedText>
           {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">
-            npm run reset-project
-          </ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
+          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{" "}
+          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{" "}
           <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
