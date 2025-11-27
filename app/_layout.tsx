ccec122ab949b4e9a6528e35b7010a3a5fdcd268
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2025-11-26 22:45:50 -0300

    feat: implement protected routes and add sign-in screen; restructure tab layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,7 +15,8 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(protected)" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-in" />
         <Stack.Screen name="modal" options={{ presentation: "modal", title: "Modal" }} />
       </Stack>
       <StatusBar style="auto" />
