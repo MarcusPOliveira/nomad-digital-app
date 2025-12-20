d42a9b708a207acba33ad47c1f5eb82e73f5de86
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2025-12-20 15:58:27 -0300

    feat: add scrollable option to ScreenContainer for enhanced layout flexibility

--- src/components/ScreenContainer.tsx
+++ src/components/ScreenContainer.tsx
@@ -1,9 +1,15 @@
+import { ScrollView, View } from "react-native"
 import { Box, BoxProps } from "./Box"
 
-export function ScreenContainer({ children, ...boxProps }: React.PropsWithChildren & BoxProps) {
+export function ScreenContainer({
+  children,
+  scrollable = false,
+  ...boxProps
+}: React.PropsWithChildren & BoxProps & { scrollable?: boolean }) {
+  const Container = scrollable ? ScrollView : View
   return (
     <Box backgroundColor="background" paddingHorizontal="padding" flex={1} {...boxProps}>
-      {children}
+      <Container>{children}</Container>
     </Box>
   )
 }
